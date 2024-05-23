bd34675d446e965143eb0b00ecf9d97f8f3f6b02
Marco Burgos <marcoburgosch2014@gmail.com>
2024-05-22 23:51:35 -0700

    Ultimo Commit

--- subo.bat
+++ subo.bat
@@ -3,4 +3,4 @@ git commit -m "Ultimo Commit"
 git push
 go build main.go
 del main.zip
-C:\Windows\WinSxS\wow64_bsdtar_31bf3856ad364e35_10.0.19041.3636_none_d51a4d5755b60f24\tar.exe -a -cf main.zip main
+tar.exe -a -cf main.zip main
